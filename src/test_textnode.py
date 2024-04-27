f36a1936868b64c05f4be9c47b3969c1a8794882
LoreviQ <lorevi@protonmail.com>
2024-04-27 22:32:07 +0100

    Added test for split function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,18 +1,27 @@
 import unittest
 
-from textnode import TextNode
+import textnode
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        node = textnode.TextNode("This is a text node", "bold")
+        node2 = textnode.TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
 
     def test_repr(self):
-        node = TextNode("This is a text node", "bold", "https://www.test.com")
+        node = textnode.TextNode("This is a text node", "bold", "https://www.test.com")
         expected = "TextNode(This is a text node, bold, https://www.test.com)"
         self.assertEqual(repr(node), expected)
+
+    def test_split(self):
+        node = textnode.TextNode("This is a node with **bold** text", "text")
+        expected = [
+            textnode.TextNode("This is a node with ", "text"),
+            textnode.TextNode("bold", "bold"),
+            textnode.TextNode(" text", "text"),
+        ]
+        self.assertEqual(textnode.split_TextNode([node], "bold"), expected)
 
 
 if __name__ == "__main__":

b7cdf1f1f13625461ec8126efb0ca4b0e0eccfb0
LoreviQ <lorevi@protonmail.com>
2024-05-07 23:24:53 +0100

    Added clause to delete file if path is a file

--- src/main.py
+++ src/main.py
@@ -8,7 +8,10 @@ def recursiveCopy():
 
 
 def recursiveDelete(path):
-    print(os.path.exists(path))
+    if not os.path.exists(path):
+        raise ValueError("Invalid Path")
+    if os.path.isfile(path):
+        os.remove(path)
 
 
 if __name__ == "__main__":
